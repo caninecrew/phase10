c7d17472ed30ce7bbf7e32d763b11298bbf95689
caninecrew <samuelleerumbley@gmail.com>
2025-05-26 11:51:54 -0500

    Refactor Deck and Card classes for improved card initialization and string representation

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.discard_pile = []
+        self.build_deck()
+        self.shuffle()
+    
+    def build_deck(self):
+        colors = ['red', 'green', 'blue', 'yellow']
+        for color in colors:
+            for number in range(1,13):
+                self.cards.append(Card('number', color, number))
+                self.cards.append(Card('number', color, number))
+            for _ in range(8):
+                self.cards.append(Card('skip', color))
+            for _ in range(4):
+                self.cards.append(Card('wild', color))
+
 class Card:
     _id_counter = 0
 
-    def __init__(self, card_type, color=None, number=None):
+    def __init__(self, card_type, color, number=None):
         self.card_type = card_type
         self.color = color
         self.number = number
@@ -12,9 +30,9 @@ class Card:
         if self.card_type == 'number':
             return f"{self.color.capitalize()} {self.number} (ID: {self.id})"
         elif self.card_type == 'skip':
-            return f"Skip (ID: {self.id})"
+            return f"{self.color.capitalize()} Skip (ID: {self.id})"
         elif self.card_type == 'wild':
-            return f'Wild (ID: {self.id})'
+            return f'{self.color.capitalize()} Wild (ID: {self.id})'
         else:
             return f"Unknown Card (ID: {self.id})"
         
