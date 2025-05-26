27a738a8cc57eeb27cdad96e08ddde027a9ede22
caninecrew <samuelleerumbley@gmail.com>
2025-05-26 13:02:09 -0500

    Add LaidDownSet class with initialization for cards and set type

---
+++ laiddownset.py
@@ -0,0 +1,5 @@
+class LaidDownSet:
+    def __init__(self, cards, set_type):
+        self.cards = cards  # list of Card objects
+        self.set_type = set_type  # e.g., 'set', 'run', 'color'
+        
