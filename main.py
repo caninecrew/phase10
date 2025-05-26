33b64b3cc94a8c9a2d1be956631c49dceeb6b05b
caninecrew <samuelleerumbley@gmail.com>
2025-05-26 12:06:29 -0500

    Refactor Card class into its own module for better organization and maintainability

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from card import Card
 class Deck:
     def __init__(self):
         self.cards = []
@@ -38,27 +39,3 @@ class Deck:
     def discard(self, card):
         self.discard_pile.append(card)
 
-class Card:
-    _id_counter = 0
-
-    def __init__(self, card_type, color=None, number=None):
-        self.card_type = card_type
-        self.color = color
-        self.number = number
-        self.id = Card._id_counter
-        Card._id_counter += 1
-
-    def __str__(self):
-        if self.card_type == 'number':
-            return f"{self.color.capitalize()} {self.number} (ID: {self.id})"
-        elif self.card_type == 'skip':
-            return f"Skip (ID: {self.id})"
-        elif self.card_type == 'wild':
-            return f'Wild (ID: {self.id})'
-        else:
-            return f"Unknown Card (ID: {self.id})"
-        
-    def __repr__(self):
-        return f"Card({self.card_type!r}, color={self.color!r}, number={self.number!r}, id={self.id})"
-
-    
